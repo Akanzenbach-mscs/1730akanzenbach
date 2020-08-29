672234c754b4a88c0e538e0dd3b611f6b06373d6
Akanzenbach-mscs <70415963+Akanzenbach-mscs@users.noreply.github.com>
2020-08-28 22:22:54 -0500

    Add files via upload

--- 1730akanzenbach/Program.cs
+++ 1730akanzenbach/Program.cs
@@ -16,7 +16,7 @@ namespace _1730akanzenbach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new akanzenbach1b1());
         }
     }
 }
--- 1730akanzenbach/Properties/AssemblyInfo.cs
+++ 1730akanzenbach/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("48b5fbe5-f453-427e-8c57-f85fa37862d9")]
+[assembly: Guid("6562c47a-e970-4a15-b0d2-ec0bfb97742b")]
 
 // Version information for an assembly consists of the following four values:
 //
